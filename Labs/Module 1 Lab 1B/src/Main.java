89ef890ea2cfd64435b250cb6efdea8f6e466bbb
Clayton Black <cblack@fanatics.com>
2019-09-01 21:10:43 -0400

    Update Main.java

--- Labs/Module 1 Lab 1B/src/Main.java
+++ Labs/Module 1 Lab 1B/src/Main.java
@@ -1,3 +1,11 @@
+/*
+* Name: Clayton Black
+* Date: 09-01-2019
+* Assignment Name: Module 1 Lab 1B
+* Assignment Brief: 2d array totaling and averaging
+* Sources:
+*/
+
 public class Main {
 
     public static void main(String[] args) {
