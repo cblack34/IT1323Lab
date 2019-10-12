1b35f9c01f2a75d7578a9cc29a851e6a722ba34d
Clayton Black <cblack@fanatics.com>
2019-10-11 22:37:13 -0400

    Update Main.java

--- Labs/Module 4 Lab 4A/src/Main.java
+++ Labs/Module 4 Lab 4A/src/Main.java
@@ -7,9 +7,41 @@
 *   -
 */
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2){
+            System.out.println("improper args");
+            System.out.println("Usage: java Main source destination");
+            System.exit(1);
+        }
+
+        File sourceFile = new File(args[0]);
+        File destinationFile = new File(args[1]);
+
+        if(!sourceFile.exists()){
+            System.out.println("Source File Does Not Exist!");
+            throw new IOException("Source File Does Not Exist!");
+        }
+        if (destinationFile.exists()){
+            System.out.println("Destination file already exist.\nPlease delete it or choose another destination");
+            throw new IOException("Destination File Already Exist!");
+        }
+
+        try (
+                Scanner lines = new Scanner(sourceFile);
+                PrintWriter output = new PrintWriter(destinationFile);
+                ) {
+            int count = 1;
+            while (lines.hasNext()){
+                output.printf("%d. %s\n", count++, lines.nextLine());
+            }
+        }
 
     }
 }
