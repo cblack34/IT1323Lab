78d1e4130485ad48b68b3c4d5cb5eb18461d7333
Clayton Black <cblack@fanatics.com>
2019-10-22 23:18:43 -0400

    Update Main.java

--- Labs/Module 5 Lab 5A/src/Main.java
+++ Labs/Module 5 Lab 5A/src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[] input = {1, 2, 3, 4};
+
+	    RecursiveSum adder = new RecursiveSum(input);
+
+        System.out.println(adder);
+        System.out.println(Arrays.toString(adder.getX()));
+        System.out.println(adder.sum(adder.getX()));
+
+        System.out.println("\nSet x to {5, 6, 7, 8}");
+        adder.setX(new int[]{5, 6, 7, 8});
+        System.out.println(adder);
+        System.out.println(Arrays.toString(adder.getX()));
+        System.out.println(adder.sum(adder.getX()));
+
+        System.out.println("\n\nCreate 2 more RecursiveSum objects: adder2.x == {1, 2, 3, 4} and adder3.x == {1, 2, 3, 4}");
+        RecursiveSum adder2 = new RecursiveSum(new int[]{1, 2, 3, 4});
+        RecursiveSum adder3 = new RecursiveSum(new int[]{1, 2, 3, 4});
+
+        System.out.println("\nadder  == adder2: " + adder.equals(adder2));
+        System.out.println("adder  == adder3: " + adder.equals(adder3));
+        System.out.println("adder2 == adder3: " + adder2.equals(adder3));
     }
 }
