a1af335f555034a5d690032ffe3739ac4e1e6cb9
Clayton Black <cblack@fanatics.com>
2019-11-08 18:16:22 -0500

    Update Game.java

--- Labs/Module 6 Lab 6A/src/Game.java
+++ Labs/Module 6 Lab 6A/src/Game.java
@@ -1,2 +1,35 @@
+/*
+ * Name: Clayton Black
+ * Date: 11-08-2019
+ * Assignment Name: Module 6 Lab 6A
+ * Assignment Brief: inheritance
+ * Sources:
+ * - Book Chapter 11
+ */
+
 public class Game {
+    String description;
+
+    public Game() {
+        this("This Game has no description");
+    }
+
+    public Game(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "description='" + description + '\'' +
+                '}';
+    }
 }
