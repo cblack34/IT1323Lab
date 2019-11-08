9eb19f369aa96bb268f7e31bc550457c9b39070a
Clayton Black <cblack@fanatics.com>
2019-11-08 18:16:35 -0500

    Update Main.java

--- Labs/Module 6 Lab 6A/src/Main.java
+++ Labs/Module 6 Lab 6A/src/Main.java
@@ -1,6 +1,24 @@
+/*
+* Name: Clayton Black
+* Date: 11-08-2019
+* Assignment Name: Module 6 Lab 6A
+* Assignment Brief: inheritance
+* Sources:
+* - Book Chapter 11
+*/
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Trivia t1 = new Trivia("Trivia Game 1", 1, 1.0, 2);
+        Trivia t2 = new Trivia("Trivia Game 2", 2, 1.0, 2);
+        Trivia t3 = new Trivia("Trivia Game 3", 3, 1.0, 2);
+        Trivia t4 = new Trivia("Trivia Game 4", 4, 1.0, 2);
+        Trivia t5 = new Trivia("Trivia Game 5", 5, 1.0, 2);
+
+        System.out.println(t1);
+        System.out.println(t2);
+        System.out.println(t3);
+        System.out.println(t4);
+        System.out.println(t5);
     }
 }
