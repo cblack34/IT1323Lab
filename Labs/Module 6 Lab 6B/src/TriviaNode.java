368308a2b9cb8b98a6c7e45e2f9b420161add716
Clayton Black <cblack@fanatics.com>
2019-11-14 19:52:15 -0500

    Update TriviaNode.java

--- Labs/Module 6 Lab 6B/src/TriviaNode.java
+++ Labs/Module 6 Lab 6B/src/TriviaNode.java
@@ -1,2 +1,37 @@
-public class TriviaNode {
+/*
+* Name: Clayton Black
+* Date: 11-14-2019
+* Assignment Name: Module 6 Lab 6B
+* Assignment Brief: Linked List
+* Sources:
+* - Book Chapter 24
+*/
+
+public class TriviaNode<E> {
+    public Trivia element;
+    public TriviaNode<E> next;
+
+    public TriviaNode(Trivia element) {
+        this.element = element;
+    }
+
+    public Trivia getElement() {
+        return element;
+    }
+
+    public TriviaNode<E> getNext() {
+        return next;
+    }
+
+    public void setNext(TriviaNode<E> next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "TriviaNode{" +
+                "element=" + element +
+                ", next=" + next +
+                '}';
+    }
 }
